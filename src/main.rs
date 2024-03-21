cdb5b8cc322386870be3485c882cd8cfd3a112e7
IsmaelTerreno <elismakof2002@gmail.com>
2024-03-21 18:36:20 -0300

    Add ApiUrl enum and get_api_url function
    Introduced a new enum, ApiUrl, to encapsulate API endpoints and a new function, get_api_url, to map these endpoints to their respective URLs. This update should streamline the handling of URLs across the application and improve endpoint management. The Rocket web server setup has also been updated to include new routes.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 #[macro_use] extern crate rocket;
+enum ApiUrl {
+    GetNotificationsByUserId(String),
+    DeleteNotificationById(String)
+}
 
+fn get_api_url(get_url: ApiUrl) -> String {
+    match get_url {
+        ApiUrl::GetNotificationsByUserId(_) => "/api/v1/notification/<user_id>".to_owned(),
+        ApiUrl::DeleteNotificationById(_) => "/api/v1/notification/delete/<user_id>".to_owned()
+    }
+}
 
 
 #[get("/api/v1/notification/<user_id>")]
@@ -7,7 +17,13 @@ fn getNotificationsByUserId(user_id: &str) -> String {
     format!("Hello, {}!", user_id)
 }
 
+#[get("/api/v1/notification")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    // Configures and launches the Rocket web server.
+    rocket::build().mount("/", routes![index, getNotificationsByUserId])
 }
