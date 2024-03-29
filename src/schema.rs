c61ed00878f4aaf9303a61f3f1dc1e8378229664
IsmaelTerreno <elismakof2002@gmail.com>
2024-03-28 22:08:19 -0300

    RJ-38: Add Diesel configuration and schema files
    The new files diesel.toml and src/schema.rs are added to manage the database schema and migrations. The diesel.toml defines settings for the Diesel CLI, and src/schema.rs contains the schema for the 'notification' table.

---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    notification (id) {
+        id -> Uuid,
+        topic -> Varchar,
+        body -> Text,
+        read -> Bool,
+    }
+}
